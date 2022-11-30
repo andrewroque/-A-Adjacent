f2bd3ac484e2dcb356faf5c5cff6a7ea58388a3c
andrewroque <78003117+andrewroque@users.noreply.github.com>
2022-11-30 09:47:41 -0800

    Test file added

--- js/app.js
+++ js/app.js
@@ -59,3 +59,45 @@ function changeImage(){
 // function divClick(){
 //     console.log('click');
 // }
+
+//CANVAS TEST
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+// const canvasOffsetX = canvas.offsetLeft;
+// const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
